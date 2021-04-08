8c9f33fd60e162b62910798aee5109c244adf5e7
Ijay van der Walt <ijayvanderwalt@users.noreply.github.com>
2021-04-08 15:50:58 +0200

    Completed the calculator

--- calculator-net-core/Controllers/HomeController.cs
+++ calculator-net-core/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -32,6 +33,24 @@ namespace calculator_net_core.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpGet("/evaluate/{expression}")]
+        public IActionResult Evaluate(string expression)
+        {
+            DataTable data = new DataTable();
+            try
+            {
+                //I had to do my own encoding for the operators as they are reserved characters which are not allowed in URL's
+                //Furthermore I used the simplest method to evaluate a string expression which is datatable.Compute similar to Js eval()
+                var result = data.Compute(
+                    expression.Replace("plus","+").Replace("minus","-").Replace("divide","/").Replace("multiply","*"), "");
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return Ok("There seems to be a syntax error!");
+            }
+            
         }
     }
 }
